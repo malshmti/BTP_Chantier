64d20cf52554b7728e42dcf2cf3ce73326a291a3
Ayoub <ayoub.moustakim@axiome-solution.fr>
2021-11-30 11:02:39 +0100

    Readme modifié

--- src/Controller/Api/ChantierController.php
+++ src/Controller/Api/ChantierController.php
@@ -11,7 +11,6 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\CoursRepository;
-use App\Entity\AvisCours;
 use App\Entity\Cours;
 use DateTime;
 
@@ -36,117 +35,11 @@ class ChantierController extends AbstractController
     {
         if (is_null($chantier)) {
             return $this->json([
-                'message' => 'Ce cours est introuvable',
+                'message' => 'Ce chantier est introuvable',
             ], 404);
         }
 
         return $this->json($chantier);
-    }
-
-    /**
-     * @Route("/jour/{day}", name="showByDay", methods={"GET"})
-     */
-    public function showByDay(string $day, CoursRepository $repository): JsonResponse
-    {
-        $format = "Y-m-d";
-        $date = DateTime::createFromFormat($format, $day);
-        $cours = $repository->getByDate($date);
-
-        return $this->json($cours, 200);
-    }
-
-    /**
-     * @Route("/{id}/avis", name="index_avis", methods={"GET"})
-     */
-    public function indexAvis(Cours $cours = null): JsonResponse
-    {
-        if (is_null($cours)) {
-            return $this->json([
-                'message' => 'Ce cours est introuvable',
-            ], 404);
-        }
-
-        return $this->json($cours->getAvisCours()->toArray());
-    }
-
-    /**
-     * @Route("/{id}/avis", name="create_avis", methods={"POST"})
-     */
-    public function createAvis(Request $request, Cours $cours = null, ValidatorInterface $validator, 
-    EntityManagerInterface $em): JsonResponse
-    {
-        if (is_null($cours)) {
-            return $this->json([
-                'message' => 'Ce cours est introuvable',
-            ], 404);
-        }
-
-        $data = json_decode($request->getContent(), true);
-        $data['cours'] = $cours;
-        $avis = new AvisCours($data);
-
-        $errors = $validator->validate($avis);
-
-        if ($errors->count() > 0) {
-            return $this->json($this->formatErrors($errors), 400);
-        }
-
-        $em->persist($avis);
-        $em->flush();
-
-        return $this->json($avis, 201);
-    }
-
-    /**
-     * @Route("/avis/{id}", name="delete_avis", methods={"DELETE"})
-     */
-    public function deleteAvis(AvisCours $avis = null, EntityManagerInterface $em): JsonResponse
-    {
-        if (is_null($avis)) {
-            return $this->json([
-                'message' => 'Cet avis est introuvable',
-            ], 404);
-        }
-
-        $em->remove($avis);
-        $em->flush();
-
-        return $this->json(null , 204);
-    }
-
-    /**
-     * @Route("/avis/{id}", name="update_avis", methods={"PATCH"})
-     */
-    public function updateAvis(Request $request, AvisCours $avis = null, ValidatorInterface $validator, EntityManagerInterface $em)
-    {
-        if (is_null($avis)) {
-            return $this->json([
-                'message' => 'Cet avis est introuvable',
-            ], 404);
-        }
-
-        $data = json_decode($request->getContent(), true);
-        $errors = $avis->updateFromArray($data);
-
-        if (count($errors) > 0) {
-            $messages = [];
-            foreach ($errors as $attribute) {
-                $messages[$attribute] = "Cet attribute n'existe pas.";
-            }
-
-            return $this->json($messages, 400);
-        }
-
-        $errors = $validator->validate($avis);
-
-        if ($errors->count() > 0) {
-            return $this->json($this->formatErrors($errors), 400);
-        }
-
-        $em->persist($avis);
-        $em->flush();
-
-        return $this->json($avis, 200);
     }
 
     protected function formatErrors($errors): array
